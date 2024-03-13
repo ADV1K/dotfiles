1d82916e364b27e40aaa0a2ca4e76e46f72d6a98
Advik <adviksingh6632@gmail.com>
2024-03-13 11:25:25 +0530

    feat: bonk

--- config/fish/conf.d/aliases.fish
+++ config/fish/conf.d/aliases.fish
@@ -1,7 +1,7 @@
 # Variables
 set EDITOR      nvim
 set VISUAL      nvim
-set BROWSER     google-chrome-stable
+# set BROWSER     google-chrome-stable
 set PAGER       bat
 
 # Aliases
@@ -17,6 +17,7 @@ alias cat       bat
 alias man       "BAT_THEME='default' batman"
 alias curl      curlie
 alias grep      batgrep
+alias diff      delta
 alias grip      "grip --browser"
 alias ncdu      "ncdu --color dark -x"
 alias take      'mkdir -p $argv[1]; cd $argv[1];'
@@ -31,7 +32,10 @@ abbr s		    "source ~/.config/fish/config.fish"
 abbr ef		    "$EDITOR ~/.config/fish/config.fish"
 abbr ek		    "$EDITOR ~/.config/kitty/kitty.conf"
 abbr et		    "$EDITOR ~/.config/tmux/tmux.conf"
+abbr ey         "$EDITOR ~/.config/yabai/yabairc"
 abbr eh         "$EDITOR ~/.config/hypr/hyprland.conf"
+abbr ev         "$EDITOR ~/.config/nvim/lua/custom/init.lua"
+abbr tb         "nc termbin.com 9999"
 abbr get	    "sudo pacman -S --needed"
 abbr remove	    "sudo pacman -Rcns"
 
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,36 +2,10 @@ source ~/.config/fish/conf.d/aliases.fish
 source ~/.config/fish/conf.d/path.fish
 
 # Init shell apps
+atuin init fish | source
 direnv hook fish | source
 starship init fish | source
 zoxide init fish --cmd j | source
 
-# source homebrew; skip if not installed
-#/opt/homebrew/bin/brew shellenv | source
-
-#source ~/.asdf/asdf.fish
-#source ~/.rye/env
-
 function fish_greeting
-
 end
-
-# # >>> conda initialize >>>
-# # !! Contents within this block are managed by 'conda init' !!
-# if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
-#     eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
-# else
-#     if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
-#         . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
-#     else
-#         set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
-#     end
-# end
-# # <<< conda initialize <<<
-#
-# conda deactivate
-# # conda activate
-
-### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
-set --export --prepend PATH "/Users/advik/.rd/bin"
-### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
